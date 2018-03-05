268d76ee048e92478fd498dd812b230ff068aed9
David Tybbleus <david@mcom.se>
2018-03-06 00:18:32 +0100

    first aurelia setup

---
+++ searchresults/searchresults_aurelia/src/components/app-content/app-content.ts
@@ -0,0 +1,8 @@
+import {inject} from 'aurelia-framework';
+
+export class AppContent {
+  data: string;
+  constructor() {
+    this.data = 'test';
+  }
+};
---
+++ searchresults/searchresults_aurelia/src/resources/elements/apptest.ts
@@ -0,0 +1,10 @@
+import {bindable} from 'aurelia-framework';
+
+export class Apptest {
+  @bindable value;
+
+  valueChanged(newValue, oldValue) {
+
+  }
+}
+
